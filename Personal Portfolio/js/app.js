b6ef84b02a5003e41a89bc06576dad64d0b64dd4
fernandonv <fernandonvieira47@hotmail.com>
2020-02-18 14:07:42 -0300

    added icons

---
+++ Personal Portfolio/js/app.js
@@ -0,0 +1,13 @@
+// const projects = document.querySelectorAll(".project-description");
+
+// projects.forEach(project=>{
+//     project.parentElement.addEventListener('mouseover', () => {
+//         project.classList.add("display-none");
+//     });
+// });
+
+// projects.forEach(project => {
+//     project.parentElement.addEventListener('mouseout', () => {
+//         project.classList.remove("display-none");
+//     });
+// });
